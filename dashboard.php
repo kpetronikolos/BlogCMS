0f4ef78e90d45a00ab6c7e5a4bb6095881950f23
konstantinos <kpetronikolos@gmail.com>
2018-01-31 20:47:30 +0200

    First commit

---
+++ dashboard.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin Dashboard</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="css/adminstyles.css">
+
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-2">
+				<h1>Kostas</h1>
+
+				<ul id="side-menu" class="nav nav-pills nav-stacked">
+					<li class="active"><a href="dashboard.php">Dashboard</a></li>
+					<li><a href="#">Add New Post</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Manage Admins</a></li>
+					<li><a href="#">Comments</a></li>
+					<li><a href="#">Live Blog</a></li>
+					<li><a href="#">Logout</a></li>
+
+				</ul>
+
+			</div> 
+
+			<div class="col-sm-10">
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+				<h4>About</h4>
+				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
+			</div>
+		</div>		
+	</div>
+
+</body>
+</html>
