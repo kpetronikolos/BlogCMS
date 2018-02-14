b7c3f936f49ed16160306bcb90225b51c2ca6412
konstantinos <kpetronikolos@gmail.com>
2018-02-15 01:49:26 +0200

    Design Blog Navigation

---
+++ blog.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blog</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+	<div style="height: 10px; background-color: #27aae1;"></div>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="blog.php"><span class="glyphicon glyphicon-text-height">Blog CMS</span></a>
+			</div>	<!-- end of class navbar-header -->
+
+			<ul class="nav navbar-nav">
+	  			<li><a href="dashboard.php">Home</a></li>
+	  			<li class="active"><a href="blog.php">Blog</a></li>
+	  			<li><a href="#">About us</a></li>
+	  			<li><a href="#">Services</a></li>
+	  			<li><a href="#">Contact us</a></li>
+	  			<li><a href="#">Feature</a></li>
+	  		</ul>
+
+	  		<form action="blog.php" class="navbar-form navbar-right">
+	  			<div class="form-group">
+	  				<input type="text" class="form-control" placeholder="Search" name="Search">
+	  			</div>
+	  			<button class="btn btn-default" name="SearchButton">Go</button>
+	  		</form>
+
+		</div>	<!-- end of class container -->
+	</nav>	<!-- end of nav -->
+	<div style="height: 10px; background-color: #27aae1; margin: -20px;"></div>
+
+</body>
+</html>
