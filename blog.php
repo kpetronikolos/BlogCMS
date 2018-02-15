a69b6f60a10e10094b324ea81e430d513d8b77b6
konstantinos <kpetronikolos@gmail.com>
2018-02-15 02:05:37 +0200

    Toggle Menu

--- blog.php
+++ blog.php
@@ -16,24 +16,33 @@
 	<nav class="navbar navbar-inverse" role="navigation">
 		<div class="container">
 			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
 				<a class="navbar-brand" href="blog.php"><span class="glyphicon glyphicon-text-height">Blog CMS</span></a>
 			</div>	<!-- end of class navbar-header -->
 
-			<ul class="nav navbar-nav">
-	  			<li><a href="dashboard.php">Home</a></li>
-	  			<li class="active"><a href="blog.php">Blog</a></li>
-	  			<li><a href="#">About us</a></li>
-	  			<li><a href="#">Services</a></li>
-	  			<li><a href="#">Contact us</a></li>
-	  			<li><a href="#">Feature</a></li>
-	  		</ul>
+			<div class="collapse navbar-collapse" id="collapse">
+				<ul class="nav navbar-nav">
+		  			<li><a href="dashboard.php">Home</a></li>
+		  			<li class="active"><a href="blog.php">Blog</a></li>
+		  			<li><a href="#">About us</a></li>
+		  			<li><a href="#">Services</a></li>
+		  			<li><a href="#">Contact us</a></li>
+		  			<li><a href="#">Feature</a></li>
+		  		</ul>
 
-	  		<form action="blog.php" class="navbar-form navbar-right">
-	  			<div class="form-group">
-	  				<input type="text" class="form-control" placeholder="Search" name="Search">
-	  			</div>
-	  			<button class="btn btn-default" name="SearchButton">Go</button>
-	  		</form>
+		  		<form action="blog.php" class="navbar-form navbar-right">
+		  			<div class="form-group">
+		  				<input type="text" class="form-control" placeholder="Search" name="Search">
+		  			</div>
+		  			<button class="btn btn-default" name="SearchButton">Go</button>
+		  		</form>
+		  		</div>	<!-- end of class navbar-collapse -->
 
 		</div>	<!-- end of class container -->
 	</nav>	<!-- end of nav -->
