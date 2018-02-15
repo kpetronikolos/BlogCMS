1f46690e6fd66b5870867320cb2abed5376239fb
konstantinos <kpetronikolos@gmail.com>
2018-02-15 22:01:38 +0200

    Designing Dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,11 @@
+<?php
+
+	require_once("include/DB.php");
+	require_once("include/Sessions.php");
+	require_once("include/Functions.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,11 +20,46 @@
 
 </head>
 <body>
+	<div style="height: 10px; background-color: #27aae1;"></div>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a class="navbar-brand" href="blog.php"><span class="glyphicon glyphicon-text-height">Blog CMS</span></a>
+			</div>	<!-- end of class navbar-header -->
+
+			<div class="collapse navbar-collapse" id="collapse">
+				<ul class="nav navbar-nav">
+		  			<li><a href="dashboard.php">Home</a></li>
+		  			<li class="active"><a href="blog.php">Blog</a></li>
+		  			<li><a href="#">About us</a></li>
+		  			<li><a href="#">Services</a></li>
+		  			<li><a href="#">Contact us</a></li>
+		  			<li><a href="#">Feature</a></li>
+		  		</ul>
+
+		  		<form action="blog.php" class="navbar-form navbar-right">
+		  			<div class="form-group">
+		  				<input type="text" class="form-control" placeholder="Search" name="Search">
+		  			</div>
+		  			<button class="btn btn-default" name="SearchButton">Go</button>
+		  		</form>
+		  	</div>	<!-- end of class navbar-collapse -->
+
+		</div>	<!-- end of class container -->
+	</nav>	<!-- end of nav -->
+	<div class="Line" style="height: 10px; background-color: #27aae1;"></div>
+
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-2">
-				<h1></h1>
-
+				<br><br>
 				<ul id="side-menu" class="nav nav-pills nav-stacked">
 					<li class="active"><a href="dashboard.php"><span class="glyphicon glyphicon-th"></span>&nbsp; Dashboard</a></li>
 					<li><a href="addNewPost.php"><span class="glyphicon glyphicon-list-alt"></span>&nbsp; Add New Post</a></li>
@@ -30,36 +73,9 @@
 
 			</div>	<!-- end of col-sm-2 --> 
 
-			<div class="col-sm-10">
-				<h1>Admin Dashboard</h1>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>
-				<h4>About</h4>
-				<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus.</p>				
+			<div class="col-sm-10">				
+				<div><?php echo Message(); echo SuccessMessage(); ?></div>
+				<h1>Admin Dashboard</h1>				
 			</div>	<!-- end of col-sm-10 -->
 		</div>	<!-- end of row -->
 	</div>	<!-- end of container-fluid -->
